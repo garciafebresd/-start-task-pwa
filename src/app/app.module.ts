ecc5e7a41a2d1ff95b96535d7b579f58d8207483
Tecnico2011 <garciafebresd@gmail.com>
2020-03-28 23:04:03 -0300

    main components added, assets and plugins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NoPageFoundComponent } from './shared/no-page-found/no-page-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NoPageFoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
   ],
   imports: [
     BrowserModule,
