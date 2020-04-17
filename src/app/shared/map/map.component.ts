99090ff257cc46006793d6e7c27c6efb9b3dbfbb
Tecnico2011 <garciafebresd@gmail.com>
2020-04-17 09:56:39 -0400

    working with map component

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -7,6 +7,8 @@ import {GPX, GeoJSON, IGC, KML, TopoJSON} from 'ol/format';
 import {defaults as defaultInteractions, DragAndDrop} from 'ol/interaction';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import {XYZ, Vector as VectorSource, OSM} from 'ol/source';
+import * as Proj from 'ol/Proj';
+import {defaults as defaultControls} from 'ol/control';
 
 @Component({
   selector: 'app-map',
@@ -17,8 +19,9 @@ export class MapComponent implements OnInit {
 
   mapTilerKey = 'b78ikDGuzTGOKUTJjp33';
 
-  attributions = '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
-  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+  // attributions = '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  // '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+  attributions='';
 
   dragAndDropInteraction = new DragAndDrop({
     formatConstructors: [
@@ -34,26 +37,53 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
 
-    const urlMap = `https://api.maptiler.com/maps/streets/?key=YkTUjJOEucFxlg6wtlLd#0.56/0/0`;
+    // const urlMap = `https://api.maptiler.com/maps/streets/?key=YkTUjJOEucFxlg6wtlLd#0.56/0/0`;
+    // const map = new Map({
+    //   interactions: defaultInteractions().extend([this.dragAndDropInteraction]),
+    //   layers: [
+    //     new TileLayer({
+    //       source: new XYZ({
+    //         attributions: this.attributions,
+    //         url: 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=YkTUjJOEucFxlg6wtlLd',
+    //         maxZoom: 20
+    //       })
+    //     })
+    //   ],
+    //   target: 'map',
+    //   view: new View({
+    //     center: [0, 0],
+    //     zoom: 2
+    //   })
+    // });
+
+	// const map = new Map({
+    //   target: "map",
+    //   layers: [
+    //     new TileLayer({
+    //       source: new OSM()
+    //     })
+    //   ],
+    //   view: new View({
+    //     center: Proj.fromLonLat([0, 0]),
+    //     zoom: 10
+    //   }),
+    //   controls: defaultControls({attribution: false, zoom: false}).extend([])
+    // });
+
 
     const map = new Map({
-      interactions: defaultInteractions().extend([this.dragAndDropInteraction]),
+      target: 'map',
       layers: [
         new TileLayer({
-          source: new XYZ({
-            attributions: this.attributions,
-            url: 'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=YkTUjJOEucFxlg6wtlLd',
-            maxZoom: 20
-          })
+          source: new OSM()
         })
       ],
-      target: 'map',
       view: new View({
+        // center: ol.proj.fromLonLat([37.41, 8.82]),
         center: [0, 0],
-        zoom: 2
+        zoom: 4
       })
     });
-
   }
 
 }
